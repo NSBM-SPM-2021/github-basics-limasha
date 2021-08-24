8f752e212c7193cd26dd198725384d3ef312a384
Limasha Aswini <60962997+limasha@users.noreply.github.com>
2021-08-24 08:25:53 +0530

    orderdbhandler

--- OrderDbHandler.cs
+++ OrderDbHandler.cs
@@ -33,37 +33,38 @@ namespace CafeProject.Models
                 return false;
         }
 
-        //// 2. Get All Item List 
-        //public List<ItemList> GetItemList()
-        //{
-        //    ConnectToDb();
-        //    List<ItemList> iList = new List<ItemList>();
+        // 2. Get All Item List 
+        public List<OrderList> GetOrderList()
+        {
+            ConnectToDb();
+            List<OrderList> iList = new List<OrderList>();
 
-        //    string query = "SELECT * FROM Item";
-        //    SqlCommand cmd = new SqlCommand(query, con);
-        //    SqlDataAdapter adapter = new SqlDataAdapter(cmd);
-        //    DataTable dt = new DataTable();
+            string query = "SELECT * FROM [Order]";
+            SqlCommand cmd = new SqlCommand(query, con);
+            SqlDataAdapter adapter = new SqlDataAdapter(cmd);
+            DataTable dt = new DataTable();
 
-        //    con.Open();
-        //    adapter.Fill(dt);
-        //    con.Close();
+            con.Open();
+            adapter.Fill(dt);
+            con.Close();
 
-        //    foreach (DataRow dr in dt.Rows)
-        //    {
-        //        iList.Add(new ItemList
-        //        {
-        //            ID = Convert.ToInt32(dr["ID"]),
-        //            ItemID = Convert.ToString(dr["Item ID"]),
-        //            ItemName = Convert.ToString(dr["Item Name"]),
-        //            Description = Convert.ToString(dr["Description"]),
-        //            UnitPrice = Convert.ToDecimal(dr["Unit Price"]),
-        //            AddedDate = Convert.ToDateTime(dr["Added Date"])
-        //        });
-        //    }
-        //    return iList;
-        //}
+            foreach (DataRow dr in dt.Rows)
+            {
+                iList.Add(new OrderList
+                {
 
-        //// 3. ********** Update Item Details **********
+                    OrderId = Convert.ToString(dr["OrderId"]),
+                    ItemName = Convert.ToString(dr["Item Name"]),
+                    UnitPrice = Convert.ToDecimal(dr["Unit Price"]),
+                    Qty = Convert.ToInt32(dr["Qty"]),
+                    Total = Convert.ToDecimal(dr["Total"]),
+
+                });
+            }
+            return iList;
+        }
+
+        //// 3. Update Item Details
         //public bool UpdateItem(ItemList iList)
         //{
         //    ConnectToDb();
@@ -79,20 +80,20 @@ namespace CafeProject.Models
         //        return false;
         //}
 
-        //// 4. ********** Delete Item **********
-        //public bool DeleteItem(int id)
-        //{
-        //    ConnectToDb();
-        //    string query = "DELETE FROM Item WHERE ID = " + id;
-        //    SqlCommand cmd = new SqlCommand(query, con);
-        //    con.Open();
-        //    int i = cmd.ExecuteNonQuery();
-        //    con.Close();
+        // 4.Delete Item 
+        public bool DeleteItem(int id)
+        {
+            ConnectToDb();
+            string query = "DELETE FROM [Order] WHERE OID = " + id;
+            SqlCommand cmd = new SqlCommand(query, con);
+            con.Open();
+            int i = cmd.ExecuteNonQuery();
+            con.Close();
 
-        //    if (i >= 1)
-        //        return true;
-        //    else
-        //        return false;
-        //}
+            if (i >= 1)
+                return true;
+            else
+                return false;
+        }
     }
 }
