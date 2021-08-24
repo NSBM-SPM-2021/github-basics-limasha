d12e6768d49f345d2ca4c08b76f0ced47de92d2f
Limasha Aswini <60962997+limasha@users.noreply.github.com>
2021-08-24 08:20:11 +0530

    pointofsale controller

--- PointofSaleController.cs
+++ PointofSaleController.cs
@@ -28,5 +28,55 @@ namespace CafeProject.Controllers
             //var result = db.GetItemList();
             return Json(new { data = list }, JsonRequestBehavior.AllowGet);
         }
+
+        public ActionResult OrderIndex()
+        {
+            ViewBag.ItemList = "Item List Page";
+            OrderDbHandler IHandler = new OrderDbHandler();
+            ModelState.Clear();
+            return View(IHandler.GetOrderList());
+        }
+
+        [HttpGet]
+        public ActionResult PlaceOrder()
+        {
+           
+            return View();
+      
+        }
+
+        [HttpPost]
+        public ActionResult PlaceOrder(OrderList iList)
+        {
+            // try
+            //{
+            if (ModelState.IsValid)
+            {
+                OrderDbHandler OrderHandler = new OrderDbHandler();
+                if (OrderHandler.InsertOrder(iList))
+                {
+                    ViewBag.Message = "Order Added Successfully";
+                    ModelState.Clear();
+                }
+            }
+            return View();
+            /* }
+             catch { return View();  }*/
+        }
+
+        //  Delete Order Details
+        public ActionResult Delete(int id)
+        {
+            try
+            {
+                OrderDbHandler ItemHandler = new OrderDbHandler();
+                if (ItemHandler.DeleteItem(id))
+                {
+                    ViewBag.AlertMsg = "Order Deleted Successfully";
+                }
+                return RedirectToAction("OrderIndex");
+            }
+            catch { return View(); }
+        }
     }
 }
